75bb513f8a9fbafa229c08818cb36ba080c7c77a
Isaac Norwich <isaac.norwich@gmail.com>
2016-06-07 13:52:55 -0400

    Add output_scrape_data.py

---
+++ output_scrape_data.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  7 11:22:33 2016
+
+@author: Econ12
+"""
+
+import os
+
+os.path.abspath('mydir/myfile.txt')
+
+path = 'C:/Users/econ12/Documents/Ancestry_Scraping'
+
+file_names = os.listdir(path)
+OUTFILENAME = 'records.txt'
+
+counter = 1
+fullrecords = {}
+
+for file in file_names:
+    with open(file, 'r') as readfile:
+        listreadfile = list(readfile)
+    
+        for i in range(len(listreadfile)):          
+            first_split = listreadfile[i].split()
+            print(first_split)
+            break
+
+        
+            try:
+                if first_split[0].isdigit():
+                #checks the first string of the row for a number
+                    print(first_split[0])
+                    
+                    
+#                    recordlist = listreadfile[i].split(',')
+#                    
+#                    #testrecordlist = [y for x in recordlist for y in x.split()]                    
+#
+#                    fullrecords[counter] = testrecordlist[:4]
+#                    #fullrecords has full records
+#                    counter += 1
+            except:
+                continue
+#            
+#            
+#with open(OUTFILENAME, 'a') as outfile:
+#    for i in range(1,len(fullrecords)+1):
+#        record = '\t'.join(fullrecords[i]).join(str(i))
+#        
+#        outfile.write(record + '\n')###
