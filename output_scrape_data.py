eb0f539e6603a3a314762b0762000b057606a015
Isaac Norwich <isaac.norwich@gmail.com>
2016-06-08 12:07:10 -0400

    Actually working

--- output_scrape_data.py
+++ output_scrape_data.py
@@ -7,12 +7,14 @@ Created on Tue Jun  7 11:22:33 2016
 import os
 
 os.path.abspath('mydir/myfile.txt')
-
 path = 'C:/Users/econ12/Documents/Ancestry_Scraping/TestFiles/'
-file_names = os.listdir(path)
-for x in range(2): file_names[x] = path + file_names[x]
 
-OUTFILENAME = 'records.txt'
+file_names = [f for f in os.listdir(path) if f.endswith('.res')]
+for x in range(len(file_names)): file_names[x] = path + file_names[x]
+print(file_names)
+
+
+OUTFILENAME = path + 'records.txt'
 
 counter = 1
 fullrecords = {}
@@ -23,34 +25,29 @@ for file in file_names:
     
         for i in range(len(listreadfile)):          
             first_split = listreadfile[i].split()
-
             try:
                 if first_split[0].isdigit():
                 #checks the first string of the row for a number
-                    recordlist= []
-                    templist = listreadfile[i].split(',')
+                    recordlist = []
+                    recordlist = listreadfile[i].split(',')
+                    recordlist = [x.strip() for x in recordlist]
                     
-                    
-                    recordlist.append(templist[0].split()[0].strip())
-                    recordlist.append(templist[0].split()[1].strip())
+                    templist = recordlist[0].split(" ",1)
+                
+                    recordlist[0] = templist[0]
+                    recordlist.insert(1, templist[1])
 
-                    recordlist.append(templist[1].strip())
-                    recordlist.append(templist[2].strip())                                                      
-                    
                     fullrecords[counter] = recordlist[:4]
-                    
-                    counter += 1
-            
+
+                    counter += 1           
             except:
                continue
             
-        readfile.close()
 
-            
-with open(OUTFILENAME, 'a') as outfile:
+with open(OUTFILENAME, 'w+') as outfile:
+    outfile.truncate()    
     for i in range(1,len(fullrecords)+1):
-        record = '\t'.join(fullrecords[i])
-        outfile.write(str(i) + '\t' + record + '\n')
-       
+        record1 = '\t'.join(fullrecords[i])
+        outfile.write(str(i) + '\t' + record1 + '\n')
         
-outfile.close()
+    outfile.close()
